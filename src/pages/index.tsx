9c00bc921afc0636d92e2cdbd1ceb2c6d1ef42fe
Bryan da Silva Bruzinga <bryanbruzinga1990@gmail.com>
2021-06-10 11:52:24 -0300

    conclusão primeiro dia

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,23 @@
-
+import SafeEnvironment from "ui/components/feedback/SafeEnvironment";
+import PageTitle from "ui/components/data-display/PageTitle";
+import UserInformation from "ui/components/data-display/UserInformation";
 
 export default function Home() {
-  return <div>Olá</div>;
+  return (
+    <div>
+      <SafeEnvironment />
+      <PageTitle
+        title={"Conheça os profissionais"}
+        subtitle={
+          "Preencha seu endereço e veja todos os profissionais da sua localidade."
+        }
+      />
+      <UserInformation
+        name={"Bryan da Silva Bruzinga"}
+        picture={"https://github.com/bryanbruzinga.png"}
+        rating={3}
+        description={"Apucarana"}
+      />
+    </div>
+  );
 }
---
+++ src/ui/components/data-display/UserInformation/index.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import {
+  AvatarStyled,
+  RatingStyled,
+  UserDescription,
+  UserInformationContainer,
+  UserName,
+} from "./index.style";
+
+interface UserInformationProps {
+  picture: string;
+  name: string;
+  rating: number;
+  description?: string;
+}
+
+const UserInformation: React.FC<UserInformationProps> = (props) => {
+  return (
+    <UserInformationContainer>
+      <AvatarStyled src={props.picture}>{props.name[0]}</AvatarStyled>
+      <UserName>{props.name}</UserName>
+      <RatingStyled readOnly value={props.rating} />
+      <UserDescription>{props.description}</UserDescription>
+    </UserInformationContainer>
+  );
+};
+
+export default UserInformation;
